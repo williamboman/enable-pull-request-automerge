5c6540bf9fc62fbed82b53fceb64b63af64ea8b7
William Boman <william@redwill.se>
2022-06-16 01:10:55 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,25 @@
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -17,9 +36,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.GithubHelper = void 0;
+const core = __importStar(__nccwpck_require__(2186));
 const core_1 = __nccwpck_require__(6762);
 const https_proxy_agent_1 = __nccwpck_require__(7219);
 const Octokit = core_1.Octokit.plugin(autoProxyAgent);
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 // Octokit plugin to support the https_proxy environment variable
 function autoProxyAgent(octokit) {
     const proxy = process.env.https_proxy || process.env.HTTPS_PROXY;
@@ -57,27 +78,49 @@ class GithubHelper {
     }
     enablePullRequestAutomerge(pullRequestId, mergeMethod) {
         return __awaiter(this, void 0, void 0, function* () {
-            const params = {
-                pullRequestId: pullRequestId,
-                mergeMethod: mergeMethod
-            };
-            const query = `mutation ($pullRequestId: ID!, $mergeMethod: PullRequestMergeMethod!) {
-      enablePullRequestAutoMerge(input: {
-        pullRequestId: $pullRequestId,
-        mergeMethod: $mergeMethod
-      }) {
-        pullRequest {
-          autoMergeRequest {
-            enabledAt
-            enabledBy {
-              login
+            let attempts = 0;
+            // We retry here because the GitHub API may return with errors due to the
+            // PR not yet being in a state where automerge can yet be enabled. Example
+            // error messages from the API include:
+            //  - "Pull request is in unstable status"
+            //  - "Pull request is in clean status"
+            do {
+                core.debug(`I am indeed attempting. current_attmpt=${attempts}`);
+                if (attempts > 0) {
+                    core.debug(`Sleeping 5000ms before next attempt. current_attempt=${attempts}`);
+                    yield sleep(5000);
+                }
+                const params = {
+                    pullRequestId: pullRequestId,
+                    mergeMethod: mergeMethod
+                };
+                const query = `mutation ($pullRequestId: ID!, $mergeMethod: PullRequestMergeMethod!) {
+        enablePullRequestAutoMerge(input: {
+          pullRequestId: $pullRequestId,
+          mergeMethod: $mergeMethod
+        }) {
+          pullRequest {
+            autoMergeRequest {
+              enabledAt
+              enabledBy {
+                login
+              }
             }
           }
         }
-      }
-    }`;
-            const response = yield this.octokit.graphql(query, params);
-            return response.enablePullRequestAutoMerge.pullRequest.autoMergeRequest;
+      }`;
+                try {
+                    const response = yield this.octokit.graphql(query, params);
+                    core.debug("I got response!");
+                    return response.enablePullRequestAutoMerge.pullRequest.autoMergeRequest;
+                }
+                catch (e) {
+                    core.warning(e instanceof Error ? e : e + '');
+                    continue;
+                }
+            } while (++attempts < 5);
+            core.debug("oh noes");
+            throw new Error('Failed to enable pull request automerge.');
         });
     }
 }
